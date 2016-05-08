a7daba205e94071466135b4634856906d603363b
Alex Silcock <alex@alexsilcock.net>
2016-05-08 02:30:27 +0100

    Docker setup tweaks

--- useradmin/config.php
+++ useradmin/config.php
@@ -1,7 +1,7 @@
 <?php
 $config = array();
 $config['mysql_host'] = 'localhost';
-$config['mysql_user'] = 'root';
-$config['mysql_password'] = '';
+$config['mysql_user'] = 'dev';
+$config['mysql_password'] = 'dev';
 $config['mysql_db'] = 'hackspace';
 $config['base_path'] = ''; // if in a subdir, use "/subdir" with no trailing slash
